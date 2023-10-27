c6af116ba6ec4ac05c75714024cfafc4c3a205dc
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-10-27 06:28:38 -0600

    updateToken

--- Server/src/routes/messageRoute.js
+++ Server/src/routes/messageRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createMessage, getMessages } from "../controllers/messageController.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = Router();
 
-router.post("/", createMessage);
-router.get("/:chatId", getMessages);
+router.post("/", verifyToken, createMessage);
+router.get("/:chatId", verifyToken, getMessages);
 
 export default router;
