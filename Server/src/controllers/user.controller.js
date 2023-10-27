7a595758cde533935cd26ec80c37ad4db14e0089
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-10-27 04:46:51 -0600

    update user info and image, decline request

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -1,6 +1,12 @@
 import { User } from '../db/userModel.js';
+import crypto from "crypto";
+import { AuthenticationDetails, CognitoUser, CognitoUserAttribute, CognitoUserPool } from 'amazon-cognito-identity-js';
 import { saveObj } from '../config/objectHandler.js';
 import { tipoObjeto } from '../config/constants.js';
+import { cognitoConfig } from "../config/credentials.js";
+
+const cognito = new CognitoUserPool(cognitoConfig);
+
 
 export const getUsers = async (req, res) => {
     try {
@@ -88,3 +94,117 @@ export const getUser = async (req, res) => {
         res.response(null, error.message, 400);
     }
 };
+
+export const declineRequest = async (req, res) => {
+    try{
+        const { idFriend } = req.params;
+        const { user:email } = req;
+
+        const friend = await User.findOne({ _id: idFriend }, { __v: 0, password: 0, idImage: 0 });
+
+        if (!friend) {
+            return res.response(null, "User not found", 400);
+        }
+        
+        // quitar las solicitudes pendientes y solicitudes recibidas
+        User.updateOne({ email }, { $pull: { friendRequests: friend._id } }).exec();
+
+        res.response(null, "Request rejected");
+
+    } catch (error) {
+        res.response(null, error.message, 400);
+    }
+}
+
+export const updateInfoUser = async (req, res) => {
+    
+    try
+    {
+        const { name, lastName, dpi, password } = req.body;
+        const { user:email } = req;
+
+        const userLogged = await User.findOne({ email }, { password: 1 });
+
+        const hash = crypto.createHash('sha256').update(password).digest('hex') + "s3m1s0c1a1**";
+
+        if (userLogged.password !== hash) {
+            return res.response(null, "Password incorrect", 400);
+        }
+
+        const authenticationDetails = new AuthenticationDetails({
+            Username: email,
+            Password: hash
+        });
+
+        const userData = {
+            Username: email,
+            Pool: cognito
+        };
+
+        const cognitoUser = new CognitoUser(userData);
+
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: async (result) => {
+                const attributeList = [];
+                attributeList.push(new CognitoUserAttribute(
+                    {
+                        Name: "name",
+                        Value: `${name} ${lastName}`
+                    }
+                ));
+
+                attributeList.push(new CognitoUserAttribute(
+                    {
+                        Name: "custom:dpi",
+                        Value: dpi
+                    }
+                ));
+
+                cognitoUser.updateAttributes(attributeList, (err, result) => {
+                    if (err) {
+                        return res.response(null, err.message, 400);
+                    } else {
+                        cognitoUser.signOut();
+                        User.updateOne({ email }, { name, lastName, dpi }).exec();
+                        return res.response(null, "User updated", 200);
+                    }
+                });
+            },
+            onFailure: (err) => {
+                console.log('aca');
+                res.response(null, err.message, 403);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.response(null, error.message, 400);
+    }
+
+}
+
+export const updateImageUser = async (req, res) => {
+    try {
+        const { user:email } = req;
+        const { buffer, originalname } = req.file;
+        const { password } = req.body;
+        const fileExtension = originalname.split('.').pop();
+
+        const userLogged = await User.findOne({ email }, { password: 1 });
+
+        const hash = crypto.createHash('sha256').update(password).digest('hex') + "s3m1s0c1a1**";
+
+        if (userLogged.password !== hash) {
+            return res.response(null, "Password incorrect", 400);
+        }
+
+        const { Key, Location } = await saveObj(buffer, fileExtension, tipoObjeto.IMG);
+
+        User.updateOne({ email }, { pathImage: Location, idImage: Key }).exec();
+
+        res.response({ pathImage: Location }, "Image updated", 200);
+
+    } catch (error) {
+        console.log(error);
+        res.response(null, error.message, 400);
+    }
+}
--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
 import { upload } from "../middlewares/uploadFile.js";
-import { acceptRequest, createRequest, getUser, getUsers } from "../controllers/user.controller.js";
+import { acceptRequest, createRequest, declineRequest, getUser, getUsers, updateImageUser, updateInfoUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.get("/users", verifyToken, getUsers);
 router.post("/create/request/friend/:idFriend", verifyToken, createRequest);
 router.post("/accept/request/friend/:idFriend", verifyToken, acceptRequest);
+router.post("/decline/request/friend/:idFriend", verifyToken, declineRequest);
 router.get("/user/:id", verifyToken, getUser);
+router.post("/user/update/info", verifyToken, updateInfoUser);
+router.post("/user/update/image", verifyToken, upload.single("image"), updateImageUser);
 
 export default router;
