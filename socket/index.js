5994eeacd1d592f25d0581d10da73be9bffe4849
Allen Román <2909080001301@ingenieria.usac.edu.gt>
2023-10-28 15:25:50 -0600

    Socket para el chat.

---
+++ socket/index.js
@@ -0,0 +1,40 @@
+const { Server } = require("socket.io");
+
+const io = new Server({ cors: { origin: "*" } });
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+    console.log("New client connected: ", socket.id);
+
+    socket.on("addNewUser", (userId) => {
+        !onlineUsers.some((user) => user.userId === userId) &&
+            onlineUsers.push({
+                userId,
+                socketId: socket.id,
+            });
+
+        console.log("Online Users: ", onlineUsers);
+        
+        io.emit("getOnlineUsers", onlineUsers);
+    });
+
+    //add new message
+    socket.on("sendMessage", (message) => {
+        const user = onlineUsers.find(
+            (user) => user.userId === message.recipientId
+            );
+
+        if (user) {
+            io.to(user.socketId).emit("getMessage", message);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+
+        io.emit("getOnlineUsers", onlineUsers);
+    });
+});
+
+io.listen(3000);
